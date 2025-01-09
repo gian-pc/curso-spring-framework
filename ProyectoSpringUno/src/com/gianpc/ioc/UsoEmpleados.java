15c1fb5e547870aa259ab675458b3e4cbb8711fc
gian <gian@pc>
2025-01-09 13:46:44 -0500

    Aplicar Inversion of Control
    - Creamos un archivo de configuración XML, el cual será el motor de
    creación de beans.
    
    - De esta forma Spring nos dará los tipos de empleados que le
    solicitemos con solo modificar el XML y NO las 4 clases como antes lo
    hacíamos.

--- ProyectoSpringUno/src/com/gianpc/ioc/UsoEmpleados.java
+++ ProyectoSpringUno/src/com/gianpc/ioc/UsoEmpleados.java
@@ -1,15 +1,23 @@
 package com.gianpc.ioc;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoEmpleados {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Creación de objetos de tipo Empleado
-		Empleados Empleado1 = new DirectorEmpleado();
+		// 1. Crear un contexto(Cargar archivo XML)
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		// Uso de los objetos creados
-		System.out.println(Empleado1.getTareas());
-
+		// 2. Pedir al Bean
+		Empleados Gian = contexto.getBean("miEmpleado",Empleados.class);
+		
+		// 3. Utilizar al Bean
+		System.out.println(Gian.getTareas());
+		
+		// Cerrar el XML
+		contexto.close();
+		
 	}
 }
